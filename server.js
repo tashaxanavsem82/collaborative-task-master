53f4e6f2de2e6e434ec8a18923736cba80b2518a
tashaxanavsem82 <swgwull@hotmail.com>
2025-10-04 12:01:25 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const http = require('http');
+const socketIo = require('socket.io');
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Collaborative Task Master!');
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
